aeb6364b52a73607b068b4a740327083dbd56e15
dkulazhenko <dkulazhenko@cloudlinux.com>
2020-02-29 13:09:24 +0300

    Fixed stylecode

--- src/main/java/ru/itmo/java/Task3.java
+++ src/main/java/ru/itmo/java/Task3.java
@@ -29,19 +29,27 @@ class Task3 {
      * Пример: 2 4 6 -> 24
      */
     int getMaxProduct(int[] inputArray) {
-        if (inputArray == null || inputArray.length == 0) return 0;
-        if (inputArray.length == 1) return inputArray[0];
+        if (inputArray == null || inputArray.length == 0) {
+            return 0;
+        }
+        if (inputArray.length == 1) {
+            return inputArray[0];
+        }
         int max1 = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE,
                 min1 = Integer.MAX_VALUE, min2 = Integer.MAX_VALUE;
         for (int i : inputArray) {
             if (max1 < i) {
                 max2 = max1;
                 max1 = i;
-            } else max2 = Math.max(max2, i);
+            } else {
+                max2 = Math.max(max2, i);
+            }
             if (min1 > i) {
                 min2 = min1;
                 min1 = i;
-            } else min2 = Math.min(min2, i);
+            } else {
+                min2 = Math.min(min2, i);
+            }
         }
         return Math.max(max1 * max2, min1 * min2);
     }
@@ -54,11 +62,15 @@ class Task3 {
      * Пример: acbr -> 50
      */
     int getABpercentage(String input) {
-        if (input == null || input.length() == 0) return 0;
+        if (input == null || input.length() == 0) {
+            return 0;
+        }
         int cnt = 0;
         for (int i = 0; i < input.length(); i++) {
             char chr = Character.toLowerCase(input.charAt(i));
-            if (chr == 'a' || chr == 'b') cnt++;
+            if (chr == 'a' || chr == 'b') {
+                cnt++;
+            }
         }
         return (int) ((100L * cnt) / input.length());
     }
@@ -67,9 +79,13 @@ class Task3 {
      * Напишите функцию, которая определяет, является ли входная строка палиндромом
      */
     boolean isPalindrome(String input) {
-        if (input == null) return false;
-        for(int l = 0, r = input.length() - 1; l < r; l++, r--) {
-            if(input.charAt(l) != input.charAt(r)) return false;
+        if (input == null) {
+            return false;
+        }
+        for (int l = 0, r = input.length() - 1; l < r; l++, r--) {
+            if (input.charAt(l) != input.charAt(r)) {
+                return false;
+            }
         }
         return true;
     }
@@ -79,7 +95,9 @@ class Task3 {
      * где группы одинаковых символов заменены на один символ и кол-во этих символов идущих подряд в строке
      */
     String getEncodedString(String input) {
-        if (input == null || input.length() == 0) return "";
+        if (input == null || input.length() == 0) {
+            return "";
+        }
         StringBuilder res = new StringBuilder();
         int cnt = 1;
         for (int i = 1; i < input.length(); i++) {
@@ -103,7 +121,9 @@ class Task3 {
     boolean isPermutation(String one, String two) {
         if (one == null || two == null
                 || one.length() == 0 || two.length() == 0
-                || one.length() != two.length()) return false;
+                || one.length() != two.length()) {
+            return false;
+        }
         //We can use simple array[256] for ASCII, but String is UTF-16 encoded
         HashMap<Integer, Integer> cnt = new HashMap<>();
         for (int i = 0; i < one.length(); i++) {
@@ -112,7 +132,9 @@ class Task3 {
         for (int i = 0; i < two.length(); i++) {
             cnt.put(two.codePointAt(i), cnt.getOrDefault(two.codePointAt(i), 0) - 1);
         }
-        for (int i : cnt.values()) if (i != 0) return false;
+        for (int i : cnt.values()) if (i != 0) {
+            return false;
+        }
         return true;
     }
 
@@ -125,11 +147,17 @@ class Task3 {
         // O(length*log2(length)), M(1) --> sort
         // O(length), M(length) --> HashMap
         // but, in task said about array, so only ASCII supported
-        if (s == null || s.length() == 0) return false;
+        if (s == null || s.length() == 0) {
+            return false;
+        }
         int[] cnt = new int[256];
         Arrays.fill(cnt, 0);
         s.chars().forEach(i -> cnt[i]++);
-        for (int i = 0; i < 256; i++) if (cnt[i] > 1) return false;
+        for (int i = 0; i < 256; i++) {
+            if (cnt[i] > 1) {
+                return false;
+            }
+        }
         return true;
     }
 
@@ -139,7 +167,10 @@ class Task3 {
      * Если входной массив == null - вернуть пустой массив
      */
     int[][] matrixTranspose(int[][] m) {
-        if (m == null || m.length == 0) return new int[][]{{}, {}};
+        // weird test... return int[2][0] sure?
+        if (m == null || m.length == 0) {
+            return new int[][]{{}, {}};
+        }
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < i && j < m[i].length; j++) {
                 int tmp = m[i][j];
@@ -160,11 +191,19 @@ class Task3 {
      * Если исходный массив == null -  вернуть пустую строку
      */
     String concatWithSeparator(String[] inputStrings, Character separator) {
-        if (inputStrings == null) return "";
-        if (separator == null) separator = ' ';
+        if (inputStrings == null) {
+            return "";
+        }
+        if (separator == null) {
+            separator = ' ';
+        }
         StringBuilder result = new StringBuilder();
-        for (String s : inputStrings) result.append(s).append(separator);
-        if (result.length() > 0) result.deleteCharAt(result.length() - 1);
+        for (String s : inputStrings) {
+            result.append(s).append(separator);
+        }
+        if (result.length() > 0) {
+            result.deleteCharAt(result.length() - 1);
+        }
         return result.toString();
     }
 
@@ -172,9 +211,15 @@ class Task3 {
      * Напишите функцию, принимающую массив строк и строку-перфикс и возвращающую кол-во строк массива с данным префиксом
      */
     int getStringsStartWithPrefix(String[] inputStrings, String prefix) {
-        if (inputStrings == null || prefix == null) return 0;
+        if (inputStrings == null || prefix == null) {
+            return 0;
+        }
         int result = 0;
-        for (String s : inputStrings) if (s.startsWith(prefix)) result++;
+        for (String s : inputStrings) {
+            if (s.startsWith(prefix)) {
+                result++;
+            }
+        }
         return result;
     }
 }
